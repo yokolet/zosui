003c427b8339fa40dfcea33445290d1dc080e6ce
Yoko Harada <yokolet@gmail.com>
2026-04-20 22:36:42 +0900

    implementing more w3c dom methods

--- src/main/java/zosui/nodes/Node.java
+++ src/main/java/zosui/nodes/Node.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
