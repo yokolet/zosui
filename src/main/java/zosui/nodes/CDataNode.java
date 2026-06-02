b59295923959a7dacb78af8bf6a824605c303bb1
Yoko Harada <yokolet@gmail.com>
2026-06-02 17:37:15 +0900

    implemented w3c CDATASection methods

--- src/main/java/zosui/nodes/CDataNode.java
+++ src/main/java/zosui/nodes/CDataNode.java
@@ -1,13 +1,24 @@
 package zosui.nodes;
 
+import org.w3c.dom.DOMException;
 import zosui.internal.QuietAppendable;
 
 /**
  * A Character Data node, to support CDATA sections.
  */
-public class CDataNode extends TextNode {
+public class CDataNode extends TextNode implements org.w3c.dom.CDATASection{
     public CDataNode(String text) {
         super(text);
+    }
+
+    @Override public String getNodeName() { return "#cdata-section"; }
+    @Override public String getNodeValue() { return text(); }
+    @Override public short getNodeType() { return Node.CDATA_SECTION_NODE; }
+    @Override public String getTextContent() throws DOMException { return text(); }
+    @Override public String getData() throws DOMException { return text(); }
+    @Override public int getLength() throws DOMException { return text().length(); }
+    @Override public String substringData(int offset, int count) throws DOMException {
+        return getData().substring(offset, offset + count);
     }
 
     @Override
